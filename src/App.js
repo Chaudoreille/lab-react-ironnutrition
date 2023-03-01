c93f7055af9309d393a71770cc56669adffa41c4
Chaudoreille <a.chaudoreille@gmail.com>
2023-03-01 10:35:45 +0100

    [IT 2] - FoodbBox component - done

--- src/App.js
+++ src/App.js
@@ -2,18 +2,43 @@ import { useState } from 'react';
 import './App.css';
 import foodsData from './foods.json';
 
+function FoodBox(props) {
+  const { name, image, calories, servings, setFoods } = { ...props };
+  function deleteFoodBox(name) {
+    return () => {
+      setFoods((foods) => {
+        return foods.filter((food) => food.name !== name);
+      });
+    };
+  }
+
+  return (
+    <div>
+      <p>{name}</p>
+      <img src={image} width={100} alt="" />
+      <p>
+        <span>Calories:</span> <span>{calories}</span>
+      </p>
+      <p>
+        <span>Servings:</span>
+        <span>{servings}</span>
+      </p>
+      <p>
+        <span>Total Calories:</span>
+        <span>{calories * servings}</span>
+      </p>
+      <button onClick={deleteFoodBox(name)}>Delete</button>
+    </div>
+  );
+}
+
 function App() {
   const [foods, setFoods] = useState(foodsData);
   return (
     <div className="App">
-      {foods.map((item) => {
-        return (
-          <div key={item.name}>
-            <p>{item.name}</p>
-            <img src={item.image} width={100} alt="" />
-          </div>
-        );
-      })}
+      {foods.map((item) => (
+        <FoodBox key={item.name} {...item} setFoods={setFoods} />
+      ))}
     </div>
   );
 }
