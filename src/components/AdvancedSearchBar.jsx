9037d4511cafb6cfe36e94582eb696c99f9d04a0
Chaudoreille <a.chaudoreille@gmail.com>
2023-03-02 09:57:40 +0100

    [IT 5] - search bar done

---
+++ src/components/AdvancedSearchBar.jsx
@@ -0,0 +1,72 @@
+import { Form, Input, Space } from 'antd';
+import React, { useState } from 'react'
+
+const AdvancedSearchBar = (props) => {
+    const {foods, setFoodDisplay } = {...props};
+    const [searchString, setSearchString] = useState('');
+
+    const updateSearchString = e => setSearchString(e.currentTarget.value)
+
+    function caseInsensitiveTextSearch(haystack, needle) {
+        return haystack.toLowerCase().indexOf(needle.toLowerCase()) !== -1;
+    }
+    
+    function rangeSearch(value, low, high) {
+        console.log(value, low, high)
+        return (low ? low <= value : true) && (high ? high >= value : true);
+    }
+
+    const searchFields = {
+        name: caseInsensitiveTextSearch,
+        calories: rangeSearch,
+        image: caseInsensitiveTextSearch,
+        servings: rangeSearch,
+    };
+
+    const filterDisplay = () => {
+        const searchWords = searchString.split(" ");
+        const validators = searchWords.map(token => {
+            let advancedSearch = token.split(':');
+            let key = 'name';
+
+            if (advancedSearch.length >= 2) {
+                advancedSearch[0] = advancedSearch[0].toLowerCase()
+                if (advancedSearch[0] in searchFields) {
+                    key = advancedSearch[0];
+                    advancedSearch = advancedSearch[1].split('-');
+                }
+            }
+            return (item) => {
+                return searchFields[key](item[key], ...advancedSearch)
+            };
+        });
+
+        setFoodDisplay((display) => {
+            return foods.filter((item) => {
+                return validators.reduce(
+                    (select, validator) => validator(item) || select,
+                    false)
+            });
+        });
+    };
+    
+    return (
+        <Form>
+        <Space direction='veritcal' size='middle'>
+            <Input
+            name='calories'
+            type='text'
+            onChange={updateSearchString}
+            />
+            <Input
+            name='submit'
+            type='submit'
+            value='search'
+            onClick={filterDisplay}
+            />
+        </Space>
+        </Form>
+    )
+}
+
+export default AdvancedSearchBar
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,55 +5,41 @@ const SearchBar = (props) => {
     const {foods, setFoodDisplay } = {...props};
     const [searchString, setSearchString] = useState('');
 
-    const searchFields = {
-        name: (haystack, needle) => haystack.indexOf(needle) !== -1,
-        calories: (value, low, high) =>
-          (low ? low <= value : true) && (high ? high >= value : true),
-        image: (haystack, needle) => haystack.indexOf(needle) !== -1,
-        servings: (value, low, high) =>
-          (low ? low <= value : true) && (high ? high >= value : true),
-      };
-      
-    const filterDisplay = () => {
-        const searchWords = searchString.split();
-        const validators = searchWords.map(token => {
-            const advancedSearch = token.split(':');
-            let key = 'name';
-            let search = [token];
+    const updateSearchString = e => setSearchString(e.currentTarget.value)
 
-            if (advancedSearch.length >= 2) {
-                if (advancedSearch[0] in searchFields) {
-                    key = advancedSearch[0];
-                    search = advancedSearch[1].split('-');
-                }
-            }
-            return (item) => searchFields[key](item, ...search);
-        });
-        console.log(validators)
+    function caseInsensitiveTextSearch(haystack, needle) {
+        return haystack.toLowerCase().indexOf(needle.toLowerCase()) !== -1;
+    }
+    
+    const filterDisplay = () => {
         setFoodDisplay((display) => {
+            const searchWords = searchString.split(" ");
+
             return foods.filter((item) => {
-                return validators.reduce((select, validator) => validator(item) || select, false)
+            return searchWords.reduce(
+                (select, word) => caseInsensitiveTextSearch(item.name, word) || select,
+                false)
             });
         });
     };
     
-  return (
-    <Form>
-      <Space direction='veritcal' size='middle'>
-        <Input
-          name='calories'
-          type='text'
-          onChange={setSearchString}
-          />
-        <Input
-          name='submit'
-          type='submit'
-          value='search'
-          onClick={filterDisplay}
-        />
-      </Space>
-    </Form>
-  )
+    return (
+        <Form>
+        <Space direction='veritcal' size='middle'>
+            <Input
+            name='calories'
+            type='text'
+            onChange={updateSearchString}
+            />
+            <Input
+            name='submit'
+            type='submit'
+            value='search'
+            onClick={filterDisplay}
+            />
+        </Space>
+        </Form>
+    )
 }
 
 export default SearchBar
