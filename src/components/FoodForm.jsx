24abd4c7c7cc2a8b9ab276e20b89a1c98d6d93d4
Chaudoreille <a.chaudoreille@gmail.com>
2023-03-02 10:59:45 +0100

    [IT 6] - hide form button - done

--- src/components/FoodForm.jsx
+++ src/components/FoodForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Space, Input, Form, Row, Col } from 'antd';
+import { Space, Input, Form, Row, Col, Button } from 'antd';
 
 const FoodForm = (props) => {
   const { setFoods } = { ...props };
@@ -7,6 +7,7 @@ const FoodForm = (props) => {
   const [pictureInput, setPictureInput] = useState();
   const [caloriesInput, setCaloriesInput] = useState();
   const [servingsInput, setServingsInput] = useState();
+  const [visible, setVisible] = useState(false);
   const handleNameInput = (e) => setNameInput(e.currentTarget.value);
   const handlePictureInput = (e) => setPictureInput(e.currentTarget.value);
   const handleCaloriesInput = (e) => setCaloriesInput(e.currentTarget.value);
@@ -22,53 +23,62 @@ const FoodForm = (props) => {
   };
 
   return (
-    <Form>
-      <Space direction="vertical" size="large">
-        <h2>Add Food</h2>
-        <Row gutter={[10, 10]}>
-          <Col>
-            <Input
-              addonBefore="Name"
-              name="name"
-              type="text"
-              onChange={handleNameInput}
-            />
-          </Col>
-          <Col>
-            <Input
-              addonBefore="Image"
-              name="picture"
-              type="url"
-              onChange={handlePictureInput}
-            />
-          </Col>
-          <Col>
-            <Input
-              addonBefore="Calories"
-              name="calories"
-              type="number"
-              onChange={handleCaloriesInput}
-            />
-          </Col>
-          <Col>
-            <Input
-              addonBefore="Servings"
-              name="servings"
-              type="number"
-              onChange={handleServingsInput}
-            />
-          </Col>
-        </Row>
-        <Row justify="center">
-          <Input
-            onClick={submitForm}
-            style={{ width: 'fit-content' }}
-            type="submit"
-            value="Create Food"
-          />
-        </Row>
-      </Space>
-    </Form>
+    <>
+      {visible ? (
+        <Form>
+          <Space direction="vertical" size="large">
+            <h2>Add Food</h2>
+            <Row gutter={[10, 10]}>
+              <Col>
+                <Input
+                  addonBefore="Name"
+                  name="name"
+                  type="text"
+                  onChange={handleNameInput}
+                />
+              </Col>
+              <Col>
+                <Input
+                  addonBefore="Image"
+                  name="picture"
+                  type="url"
+                  onChange={handlePictureInput}
+                />
+              </Col>
+              <Col>
+                <Input
+                  addonBefore="Calories"
+                  name="calories"
+                  type="number"
+                  onChange={handleCaloriesInput}
+                />
+              </Col>
+              <Col>
+                <Input
+                  addonBefore="Servings"
+                  name="servings"
+                  type="number"
+                  onChange={handleServingsInput}
+                />
+              </Col>
+            </Row>
+            <Row justify="center">
+              <Input
+                onClick={submitForm}
+                style={{ width: 'fit-content' }}
+                type="submit"
+                value="Create Food"
+              />
+            </Row>
+            <Button onClick={() => setVisible(false)}>Hide Form</Button>
+          </Space>
+        </Form>
+      ) : (
+        <Space direction="vertical">
+          <Button onClick={() => setVisible(true)}>Show Form</Button>
+        </Space>
+      )}
+    </>
   );
 };
 
